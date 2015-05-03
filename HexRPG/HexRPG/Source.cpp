111461e7b3df0e3b3e2f33ebab350b9e494610ae
João Miguel Lopes de Oliveira <joaomlo2@hotmail.com>
2015-05-03 19:52:45 +0100

    Pode-se controlar a posição do jogador.
    Foi adicionada a função "Main Menu", como diz o nome, será o menu
    principal a partir de onde se iniciará/carregará um jogo
    
    A função "Jogada", recebe o hexágono onde se encontra o jogador, e
    mediante o pressionar de uma tecla, o jogador desloca-se pelo mapa.
    
    Foi ainda alterada a função "Mostrar Célula" para "Actualizar
    Interface", sendo adicionadas mais informações a mostrar na consola,
    nomeadamente a célula e as suas vizinhas, as teclas numéricas que se
    podem carregar para mudar de posição (7,8,9,1,2,3) e por fim,
    informações dos recém-adicionados elementos da estrutura hexagono:
    "Acampamento","Facção","Tipo-de-Terreno","Tipo_de_Bioma","Aldeia". Para
    mais informações, consultar os comentários nas linhas respectivas no
    typedef da estrutura.

--- HexRPG/HexRPG/Source.cpp
+++ HexRPG/HexRPG/Source.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
+#include <time.h>
 
 typedef struct hexagono
 {                                       /*																			EX:Se x=5 e y=5:		 ___		*/
@@ -11,7 +13,20 @@ typedef struct hexagono
 	struct hexagono*SE;					/*							-"	"	"	6, "  "           "     (x-1,y+1)	(5-1,5+1)=(4,6)			\___/		*/
 	int x;
 	int y;
+	int Facção; //0-Cruzados 1-Mouros
+	int Tipo_de_Terreno; //0-Planície 1-Subida/Descida Suave 2-Montanha Íngreme
+	int Tipo_de_Bioma; //0-Floresta/Campo Normal 1-Tundra Gelada 2-Deserto
+	int Aldeia; //0-Não 1-Sim
+	int Acampamento; //(Inimigo) 0-Não 1-Sim
 }*HEX;
+
+int Encontrou_Celula(HEX apt, int X, int Y)
+{
+	if (apt->x == X&&apt->y == Y)
+		return 1;
+	else
+		return 0;
+}
 
 HEX Encontrar_Celula(HEX Mapa, int X, int Y)
 {
@@ -21,25 +36,72 @@ HEX Encontrar_Celula(HEX Mapa, int X, int Y)
 	}
 	else
 	{
-		if (Mapa->x == X)
+		if (Encontrou_Celula(Mapa, X, Y) == 1)
 		{
-			if (Mapa->y == Y)
+			return Mapa;
+		}
+		else
+		{
+			if (Mapa->N != NULL)
 			{
-				return Mapa;
+				Encontrar_Celula(Mapa->N, X, Y);
 			}
-			else
+			if (Mapa->NE != NULL)
 			{
-				if (Mapa->y > Y)
-				{
-
-				}
+				Encontrar_Celula(Mapa->NE, X, Y);
+			}
+			if (Mapa->NO != NULL)
+			{
+				Encontrar_Celula(Mapa->NO, X, Y);
+			}
+			if (Mapa->S != NULL)
+			{
+				Encontrar_Celula(Mapa->S, X, Y);
+			}
+			if (Mapa->SE != NULL)
+			{
+				Encontrar_Celula(Mapa->SE, X, Y);
+			}
+			if (Mapa->SO != NULL)
+			{
+				Encontrar_Celula(Mapa->SO, X, Y);
 			}
 		}
 	}
 }
 
-void Mostrar_Celula(HEX Mapa)
+void Movimentos_Possiveis(HEX Jogador)
 {
+	printf("Onde Pode ir:\n");
+	if (Jogador->N != NULL)
+		printf("    8    \n");
+	else
+		printf("    #    \n");
+	if (Jogador->NO != NULL)
+		printf("8       ");
+	else
+		printf("#       ");
+	if (Jogador->NE != NULL)
+		printf("9\n");
+	else
+		printf("#\n");
+	if (Jogador->SO != NULL)
+		printf("1       ");
+	else
+		printf("#       ");
+	if (Jogador->SE != NULL)
+		printf("3\n");
+	else
+		printf("#\n");
+	if (Jogador->S != NULL)
+		printf("    2    \n");
+	else
+		printf("    #    \n");
+}
+
+void Actualizar_Interface(HEX Mapa)
+{
+	system("cls");
 	HEX aux = Mapa;
 	if (aux == NULL)
 		printf("Essa Celula nao existe!");
@@ -48,29 +110,36 @@ void Mostrar_Celula(HEX Mapa)
 		if (aux->N != NULL)
 			printf("    %d,%d\n", aux->N->x, aux->N->y);
 		else
-			printf("    NULL\n");
+			printf("    NOPE\n");
 		if (aux->NO != NULL)
 			printf("%d,%d	", aux->NO->x, aux->NO->y);
 		else
-			printf("NULL	");
+			printf("NOPE	");
 		if (aux->NE != NULL)
 			printf("%d,%d\n", aux->NE->x, aux->NE->y);
 		else
-			printf("NULL\n");
+			printf("NOPE\n");
 		printf("    %d,%d    	\n", aux->x, aux->y);
 		if (aux->SO != NULL)
 			printf("%d,%d     ",aux->SO->x, aux->SO->y);
 		else
-			printf("NULL	");
+			printf("NOPE	");
 		if (aux->SE != NULL)
 			printf("%d,%d\n", aux->SE->x, aux->SE->y);
 		else
-			printf("NULL\n");
+			printf("NOPE\n");
 		if (aux->S != NULL)
 			printf("    %d,%d\n", aux->S->x, aux->S->y);
 		else
-			printf("    NULL\n\n");
+			printf("    NOPE\n\n");
 	}
+	printf("Este Hexagono encontra-se nas coordenadas (%d,%d).\n",Mapa->x,Mapa->y);
+	Movimentos_Possiveis(Mapa);
+	printf("%d\n", aux->Acampamento);
+	printf("%d\n", aux->Aldeia);
+	printf("%d\n", aux->Facção);
+	printf("%d\n", aux->Tipo_de_Bioma);
+	printf("%d\n", aux->Tipo_de_Terreno);
 }
 
 void Actualizar_Ligaçoes(HEX Centro)
@@ -171,6 +240,11 @@ HEX Inicializar(HEX Mapa)
 		Mapa = (HEX)malloc(sizeof(struct hexagono));
 		Mapa->x = 0;
 		Mapa->y = 0;
+		Mapa->Acampamento = rand() % 10;
+		Mapa->Facção = 0;
+		Mapa->Aldeia = 1;
+		Mapa->Tipo_de_Bioma = 0;
+		Mapa->Tipo_de_Terreno = 0;
 		Mapa->N = NULL;
 		Mapa->NE = NULL;
 		Mapa->NO = NULL;
@@ -200,6 +274,11 @@ void Add_Hex(HEX apt,int dir)
 			aux->SO = NULL;
 			aux->S = NULL;
 			aux->NE = NULL;
+			aux->Acampamento = 0;
+			aux->Aldeia = 0;
+			aux->Facção = 0;
+			aux->Tipo_de_Bioma = 0;
+			aux->Tipo_de_Terreno = 0;
 			apt->NO = aux;
 			aux->SE = apt;
 		}
@@ -221,6 +300,11 @@ void Add_Hex(HEX apt,int dir)
 			aux->NE = NULL;
 			aux->SE = NULL;
 			aux->SO = NULL;
+			aux->Acampamento = 0;
+			aux->Aldeia = 0;
+			aux->Facção = 0;
+			aux->Tipo_de_Bioma = 0;
+			aux->Tipo_de_Terreno = 0;
 			apt->N = aux;
 			aux->S = apt;
 		}
@@ -242,6 +326,11 @@ void Add_Hex(HEX apt,int dir)
 			aux->SE = NULL;
 			aux->S = NULL;
 			aux->NO = NULL;
+			aux->Acampamento = 0;
+			aux->Aldeia = 0;
+			aux->Facção = 0;
+			aux->Tipo_de_Bioma = 0;
+			aux->Tipo_de_Terreno = 0;
 			apt->NE = aux;
 			aux->SO = apt;
 		}
@@ -263,6 +352,11 @@ void Add_Hex(HEX apt,int dir)
 			aux->SE = NULL;
 			aux->SO = NULL;
 			aux->NE = NULL;
+			aux->Acampamento = 0;
+			aux->Aldeia = 0;
+			aux->Facção = 0;
+			aux->Tipo_de_Bioma = 0;
+			aux->Tipo_de_Terreno = 0;
 			apt->SE = aux;
 			aux->NO = apt;
 		}
@@ -284,6 +378,11 @@ void Add_Hex(HEX apt,int dir)
 			aux->SE = NULL;
 			aux->SO = NULL;
 			aux->NE = NULL;
+			aux->Acampamento = 0;
+			aux->Aldeia = 0;
+			aux->Facção = 0;
+			aux->Tipo_de_Bioma = 0;
+			aux->Tipo_de_Terreno = 0;
 			apt->S = aux;
 			aux->N = apt;
 		}
@@ -305,6 +404,11 @@ void Add_Hex(HEX apt,int dir)
 			aux->SE = NULL;
 			aux->SO = NULL;
 			aux->NO = NULL;
+			aux->Acampamento = 0;
+			aux->Aldeia = 0;
+			aux->Facção = 0;
+			aux->Tipo_de_Bioma = 0;
+			aux->Tipo_de_Terreno = 0;
 			apt->SO = aux;
 			aux->NE = apt;
 		}
@@ -316,16 +420,108 @@ void Add_Hex(HEX apt,int dir)
 	Actualizar_Ligaçoes(apt);
 }
 
+void Main_Menu()
+{
+	char a;
+	printf("=================================================================================                                   HEX RPG                                    =================================================================================\n1-Comecar\n2-Continuar\n3-Sair\n");
+	while (!_kbhit())
+	{}
+	switch (_getch())
+	{
+	case 49:
+		system("cls");
+		printf("Novo Jogo\n");
+		break;
+	case 50:
+		system("cls");
+		printf("Continuar Jogo\n");
+		break;
+	case 51:
+		system("cls");
+		printf("Acabar Jogo\n");
+	}
+}
+
+HEX Jogada(HEX Jogador)
+{
+	HEX aux=NULL;
+	switch (_getch())
+	{
+	case 55:
+		if (Jogador->NO != NULL)
+			aux = Jogador->NO;
+		else
+			printf("Essa celula nao existe!");
+		break;
+	case 56:
+		if (Jogador->N != NULL)
+			aux = Jogador->N;
+		else
+			printf("Essa celula nao existe!");
+		break;
+	case 57:
+		if (Jogador->NE != NULL)
+			aux = Jogador->NE;
+		else
+			printf("Essa celula nao existe!");
+		break;
+	case 49:
+		if (Jogador->SO != NULL)
+			aux = Jogador->SO;
+		else
+			printf("Essa celula nao existe!");
+		break;
+	case 50:
+		if (Jogador->S != NULL)
+			aux = Jogador->S;
+		else
+			printf("Essa celula nao existe!");
+		break;
+	case 51:
+		if (Jogador->SE != NULL)
+			aux = Jogador->SE;
+		else
+			printf("Essa celula nao existe!");
+		break;
+	default:
+		printf("Essa Jogada nao e valida|");
+		break;
+	}
+	return aux;
+}
 
 void main()
 {
-	HEX Mapa = NULL;
+	srand(time(NULL));
+	int Perdeu = 0;
+	HEX Mapa = NULL,Jogador;
 	Mapa = Inicializar(Mapa);
-	Mostrar_Celula(Mapa);
+	Jogador = Mapa;
 	Add_Hex(Mapa, 1);
 	Add_Hex(Mapa, 2);
-	Mostrar_Celula(Mapa);
-	Mostrar_Celula(Mapa->N);
+	Add_Hex(Mapa, 3);
+	Add_Hex(Mapa, 4);
+	Add_Hex(Mapa, 5);
+	Add_Hex(Mapa, 6);
+	Add_Hex(Mapa->N, 1);
+	Add_Hex(Mapa->N, 2);
+	Add_Hex(Mapa->N, 3);
+	Add_Hex(Mapa->NO, 1);
+	Add_Hex(Mapa->NO, 6);
+	Add_Hex(Mapa->SO, 6);
+	Add_Hex(Mapa->S, 6);
+	Add_Hex(Mapa->S, 5);
+	Add_Hex(Mapa->S, 4);
+	Add_Hex(Mapa->SE, 4);
+	Add_Hex(Mapa->NE, 3);
+	Add_Hex(Mapa->NE, 4);
+	Actualizar_Interface(Jogador);
+	while (Perdeu==0)
+	{
+		Jogador=Jogada(Jogador);
+		Actualizar_Ligaçoes(Jogador);
+		Actualizar_Interface(Jogador);
+	}
 	system("pause");
 	free(Mapa);
 }
